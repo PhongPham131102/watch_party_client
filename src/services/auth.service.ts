efafa9112647a4576be7bc5614dbaf7c27176ce9
PhongPham131102 <phongpham131102@gmail.com>
2025-12-13 21:06:41 +0700

    feat: Add GetMe response type and refactor auth types for consistency. Update AuthService to include getMe method for user retrieval.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,24 +1,41 @@
 import { ApiResponse } from "../types";
 import { apiClient } from "./api.service";
-import { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from "@/src/types/auth.types";
+import {
+  GetMeReponse,
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+  RegisterResponse,
+} from "@/src/types/auth.types";
 
 export class AuthService {
-    async register(data: RegisterRequest): Promise<RegisterResponse> {
-        try {
-            const response = await apiClient.post<RegisterResponse>("/auth/register", data);
-            return response;
-        } catch (error) {
-            throw error as ApiResponse;
-        }
+  async register(data: RegisterRequest): Promise<RegisterResponse> {
+    try {
+      const response = await apiClient.post<RegisterResponse>(
+        "/auth/register",
+        data
+      );
+      return response;
+    } catch (error) {
+      throw error as ApiResponse;
     }
-    async login(data: LoginRequest): Promise<LoginResponse> {
-        try {
-            const response = await apiClient.post<LoginResponse>("/auth/login", data);
-            return response;
-        } catch (error) {
-            throw error as ApiResponse;
-        }
+  }
+  async login(data: LoginRequest): Promise<LoginResponse> {
+    try {
+      const response = await apiClient.post<LoginResponse>("/auth/login", data);
+      return response;
+    } catch (error) {
+      throw error as ApiResponse;
     }
+  }
+  async getMe(): Promise<GetMeReponse> {
+    try {
+      const response = await apiClient.get<GetMeReponse>("/auth/me");
+      return response;
+    } catch (error) {
+      throw error as ApiResponse;
+    }
+  }
 }
 
 export const authService = new AuthService();
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,33 +1,39 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { Role, UserProfile, ApiResponse, BaseEntity } from "./index";
 
 export interface FormLoginErrors {
-    username?: string;
-    password?: string;
+  username?: string;
+  password?: string;
 }
 export interface RegisterRequest {
-    email: string;
-    password: string;
-    username: string;
-    fullName: string;
+  email: string;
+  password: string;
+  username: string;
+  fullName: string;
 }
 export interface User extends BaseEntity {
-    email: string;
-    username: string;
-    isActive: boolean;
-    role: Role;
-    profile: UserProfile;
+  email: string;
+  username: string;
+  isActive: boolean;
+  role: Role;
+  profile: UserProfile;
 }
 export interface RegisterResponse extends ApiResponse {
-    data: {
-        user: User,
-        accessToken: string;
-        refreshToken: string;
-    }
+  data: {
+    user: User;
+    accessToken: string;
+    refreshToken: string;
+  };
 }
 
 export interface LoginRequest {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
-export interface LoginResponse extends RegisterResponse {
+export interface LoginResponse extends RegisterResponse {}
+
+export interface GetMeReponse extends ApiResponse {
+  data: {
+    user: User;
+  };
 }
